7f8928a43de5f4b4a1d13563fa54dbabce8a2ce4
Ashparshp <ashparsh.connects@gmail.com>
2025-02-28 03:07:37 +0530

    Update gradient colors in Working Hours page for improved visibility in dark mode

--- client/src/pages/WorkingHoursPage.jsx
+++ client/src/pages/WorkingHoursPage.jsx
@@ -223,7 +223,7 @@ const WorkingHoursPage = () => {
           <div
             className={`w-32 h-1 bg-gradient-to-r ${
               isDark
-                ? "from-indigo-500 to-indigo-300/70"
+                ? "from-white to-gray-500"
                 : "from-indigo-600 to-indigo-300"
             } rounded-full`}
           />
@@ -510,7 +510,7 @@ const WorkingHoursPage = () => {
               <div
                 className={`w-24 h-1 bg-gradient-to-r ${
                   isDark
-                    ? "from-indigo-500 to-indigo-300/70"
+                    ? "from-white to-gray-500"
                     : "from-indigo-600 to-indigo-300"
                 } mt-2 rounded-full`}
               />
